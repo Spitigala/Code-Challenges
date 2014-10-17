6bb40d28705812ff3bb182fb66c6c11e697d861d
Sahan <keeblersf@gmail.com>
2014-10-17 14:37:47 -0400

    Refactor letter changes solution to account for edge cases and accept additional arg for offset

--- letter_changes/letter_changes.rb
+++ letter_changes/letter_changes.rb
@@ -1,7 +1,44 @@
 # Take an input string and for each letter, replace with the next letter in the alphabet.
 # ie: "abc" becomes "bcd".
 
-def LetterChanges(str)
+
+def LetterChanges(input_string, offset)
+
+	alphabet = ('a'..'z').to_a
+	new_word = []
+	string = input_string.split("")
+
+	string.each do |letter|
+
+		current_letter_index = alphabet.find_index(letter)
+		counter = 0
+
+		while counter < offset
+			if current_letter_index < (alphabet.length - 1)
+				current_letter_index = current_letter_index + 1
+				counter += 1 
+			else
+				current_letter_index = 0
+				counter += 1
+			end
+		end
+
+		new_word << alphabet[current_letter_index]
+
+	end
+
+
+	return new_word.join("")
+         
+end
+
+p LetterChanges("test", 2) # => "vguv"
+
+## PREVIOUS SOLUTION
+
+=begin
+
+def LetterChanges(input_string, offset)
 
   alphabet = ('a'..'z').to_a
   new_word = []
@@ -17,4 +54,4 @@ def LetterChanges(str)
          
 end
 
-LetterChanges("test")
+=end
